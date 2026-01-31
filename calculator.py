9fc7269f7c35601964334e8a0f828fa00b311c9e
archana73073 <archanaverma96214@gmail.com>
2026-01-31 19:08:10 +0530

    Add basic calculator GUI with tkinter
    Implemented a basic calculator GUI using tkinter with various mathematical functions.

---
+++ calculator.py
@@ -0,0 +1,112 @@
+from tkinter import * #import tkinter module
+import math #import math module
+FONT = ('Arial', 12) #define fint and font size
+
+#define calculator button names
+buttons_name = ("%", "CE", "C", "←", "1/x", "x^2", "sqrt", "÷", "7", "8", "9",
+                "x", "4", "5", "6", "-", "1", "2", "3", "+", "±", "0", ".", "=")
+
+#calculator GUI class
+class CalcPage():
+    def __init__(self, master):
+        self.master = master
+        self.calc_flag = False  #check if last action was calculation
+        self.initUI()  #initialize UI
+
+    def initUI(self):
+        '''initialize user interface'''
+        
+        #entery box to show result
+        self.display = StringVar()
+        self.result = Entry(self.master,
+                            relief=RIDGE,
+                            textvariable=self.display,
+                            justify='right',
+                            font=FONT,
+                            bd=5,
+                            bg="light blue")
+        self.result.pack(side=TOP, expand=YES, fill=X)
+        #center the window
+        self.center_window()
+        #frame the buttons
+        self.buttons_frame = Frame(self.master)
+        self.buttons_frame.pack(side=BOTTOM, pady=2)
+        #create buttons
+        self.create_buttons(self.click_handler)
+
+    def center_window(self):
+        '''center window on screen'''
+
+        screen_width = self.master.winfo_screenwidth()  
+        screen_height = self.master.winfo_screenheight()  
+        x = (screen_width - 200) / 2
+        y = (screen_height - 100) / 2  
+        self.master.geometry('246x260+%d+%d' % (x, y))  
+        self.master.title("CALCULATOR")  
+
+    def create_buttons(self, click_method, names=buttons_name, cols=4):
+        '''create buttons and bind click event'''
+        for i, name in enumerate(names):
+            row, col = i // cols, i % cols
+
+            b = Button(self.buttons_frame, text=name, font=FONT, width=5)
+            b.grid(row=row, column=col)  
+            b.bind("<Button-1>", click_method)  
+
+    def click_handler(self, event):
+        '''handle button click'''
+        c = event.widget["text"]  
+        s = self.display.get()  
+        if "ERROR" in s:
+            s = ''
+        if c == "←":  
+            self.display.set(s[:-1])
+        elif c == "C" or c == "CE":
+            self.display.set('')
+        elif c == "x^2":  
+            self.display.set(eval(s)**2)
+        elif c == "sqrt":  
+            try:
+                self.display.set(math.sqrt(eval(s)))
+            except:
+                self.display.set("ERROR")
+        elif c == "1/x":  
+            try:
+                self.display.set(1 / eval(s))
+            except:
+                self.display.set("ERROR")
+        elif c == "±":  
+            if '-' in s:  
+                self.display.set(s[1:])  
+            else:  
+                self.display.set('-' + s)  
+        elif c in "0123456789.":
+            if self.calc_flag:
+                self.calc_flag = False
+                self.display.set(c)
+            else:
+                self.display.set(s + c)
+        elif c in "+-x÷":  
+            self.display.set(s + c)
+        elif c == "=":
+            self.calc()
+            self.calc_flag = True
+        self.result.icursor(len(self.display.get()))
+
+    def calc(self):
+        '''perform calculation'''
+        try:
+            s = self.display.get()
+            if "÷" in s:  
+                s = s.replace("÷", "/")
+            elif "x" in s:  
+                s = s.replace("x", "*")
+            self.display.set(eval(s))
+        except:
+            self.display.set("ERROR")
+
+import tkinter as tk
+
+window = tk.Tk()      
+CalcPage(window)      
+window.mainloop()     
